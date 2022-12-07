5b9b93052cd0cda8408ddd1dd77b8aa199e8e59d
Ty Everett <ty@tyweb.us>
2022-12-06 17:10:00 -0800

    Step #6 complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Prompt from '@babbage/react-prompt'
 
 ReactDOM.render(
-  <App />,
+  <Prompt
+    customPrompt
+    appName='ToDo List'
+    author='Peer-to-peer Privacy Systems Research, LLC'
+    authorUrl='https://projectbabbage.com'
+    description='Complete ToDo items, with a reward. This simple app demonstrates the power and potential of Bitcoin tokenization, by allowing every-day people to create and redeem ToDo outputs on a distributed ledger.'
+  >
+    <App />
+  </Prompt>,
   document.getElementById('root')
 )
